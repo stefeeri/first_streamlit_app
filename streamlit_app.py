9103bbe8b2613e21fb9225f6b8d7107276b86471
stefeeri <139182510+stefeeri@users.noreply.github.com>
2023-07-17 15:02:34 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-
 streamlit.title('My parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -27,16 +26,16 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-#Take the json response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#command to normalize the information on the screen
-streamlit.dataframe(fruityvice_normalized)
+try:
+ fruit_choice = streamlit.text_input('What fruit would you like information about?') 
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+streamlit.error()
 
 # Don't run anything here while we fix the code
 streamlit.stop()
