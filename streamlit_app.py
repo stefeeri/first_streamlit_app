65a1dcb96db2392a0ce813ddfde4ab2684d51e91
stefeeri <139182510+stefeeri@users.noreply.github.com>
2023-07-17 17:06:02 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,8 +70,6 @@ if streamlit.button('Add a Fruit to the List'):
     streamlit.text(back_from_function)
 streamlit.write('Thanks for adding ', add_my_fruit)
 
-
-
 # Don't run anything here while we fix the code
 streamlit.stop()
 
